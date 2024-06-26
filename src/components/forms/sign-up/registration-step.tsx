9856313deaea2e3f190378af2c8068bb8080771d
Sithara <sitharapitiyage@gmail.com>
2024-06-26 14:58:51 +0530

    sign up 2nd step

--- src/components/forms/sign-up/registration-step.tsx
+++ src/components/forms/sign-up/registration-step.tsx
@@ -1,18 +1,20 @@
 "use client";
+import { Spinner } from "@/components/spinner/index";
 import { useAuthContextHook } from "@/context/use-auth-context";
+import dynamic from "next/dynamic";
 import { useState } from "react";
 import { useFormContext } from "react-hook-form";
 import TypeSelectionForm from "./type-selection-form";
 
-// const DetailForm = dynamic(() => import("./account-details-form"), {
-//   ssr: false,
-//   loading: Spinner,
-// });
+const DetailForm = dynamic(() => import("./account-details-form"), {
+  ssr: false,
+  loading: Spinner,
+});
 
-// const OTPForm = dynamic(() => import("./otp-form"), {
-//   ssr: false,
-//   loading: Spinner,
-// });
+const OTPForm = dynamic(() => import("./otp-form"), {
+  ssr: false,
+  loading: Spinner,
+});
 
 type Props = {};
 
@@ -39,8 +41,8 @@ const RegistrationFormStep = (props: Props) => {
       );
     case 2:
       return <DetailForm errors={errors} register={register} />;
-    // case 3:
-    //   return <OTPForm onOTP={onOTP} setOTP={setOnOTP} />;
+    case 3:
+      return <OTPForm onOTP={onOTP} setOTP={setOnOTP} />;
   }
 
   return <div>RegistrationFormStep</div>;
