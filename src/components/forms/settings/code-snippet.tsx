bbdf4c52aedc56121e6c34e37e165b7d29b6d6bd
Sithara <sitharapitiyage@gmail.com>
2024-07-16 16:12:50 +0530

    domain setting changes

--- src/components/forms/settings/code-snippet.tsx
+++ src/components/forms/settings/code-snippet.tsx
@@ -46,7 +46,7 @@ const CodeSnippet = ({ id }: Props) => {
         label="Code snippet"
         message="Copy and paste this code snippet into the header tag of your website"
       />
-      <div className="bg-cream px-10 rounded-lg inline-block relative">
+      <div className="bg-muted px-10 rounded-lg inline-block relative">
         <Copy
           className="absolute top-5 right-5 text-gray-400 cursor-pointer"
           onClick={() => {
--- src/components/forms/settings/form.tsx
+++ src/components/forms/settings/form.tsx
@@ -2,8 +2,18 @@
 
 import { Separator } from "@/components/ui/separator";
 import { useSettings } from "@/hooks/settings/use-settings";
+import PremiumBadge from "@/icons/premium-badge";
+import dynamic from "next/dynamic";
 import CodeSnippet from "./code-snippet";
 import { DomainUpdate } from "./domain-update";
+import EditChatbotIcon from "./edit-chatbot-icon";
+
+const WelcomeMessage = dynamic(
+  () => import("./greetings-message").then((props) => props.default),
+  {
+    ssr: false,
+  }
+);
 
 type Props = {
   id: string;
@@ -32,6 +42,30 @@ function SettingsForm({ id, name, chatBot, plan }: Props) {
         <Separator orientation="horizontal" />
         <DomainUpdate name={name} register={register} errors={errors} />
         <CodeSnippet id={id} />
+      </div>
+      <div className="flex flex-col gap-3 mt-5">
+        <div className="flex gap-4 items-center">
+          <h2 className="font-bold text-2xl">Chatbot Settings</h2>
+          <div className="flex gap-1 bg-cream rounded-full px-3 py-1 text-xs items-center font-bold">
+            <PremiumBadge />
+            Premium
+          </div>
+        </div>
+        <Separator orientation="horizontal" />
+        <div className="grid md:grid-cols-2">
+          <div className="col-span-1 flex flex-col gap-5 order-last md:order-first">
+            <EditChatbotIcon
+              chatBot={chatBot}
+              register={register}
+              errors={errors}
+            />
+            <WelcomeMessage
+              message={chatBot?.welcomeMessage!}
+              register={register}
+              errors={errors}
+            />
+          </div>
+        </div>
       </div>
     </form>
   );
