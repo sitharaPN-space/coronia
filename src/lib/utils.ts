7260112a0d46c1a2e00f2ff324df22e18842f5bb
Sithara <sitharapitiyage@gmail.com>
2024-07-18 11:42:54 +0530

    messaging stuff

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,55 @@
-import { type ClassValue, clsx } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { clsx, type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
 }
+
+export const extractUUIDFromString = (url: string) => {
+  return url.match(
+    /^[0-9a-f]{8}-?[0-9a-f]{4}-?[1-5][0-9a-f]{3}-?[89ab][0-9a-f]{3}-?[0-9a-f]{12}$/i
+  );
+};
+
+export const pusherClient = {};
+// new PusherClient(
+//   process.env.NEXT_PUBLIC_PUSHER_APP_KEY as string,
+//   {
+//     cluster: process.env.NEXT_PUBLIC_PUSHER_APP_CLUSTOR as string,
+//   }
+// );
+
+export const pusherServer = {};
+//  new PusherServer({
+//   appId: process.env.NEXT_PUBLIC_PUSHER_APP_ID as string,
+//   key: process.env.NEXT_PUBLIC_PUSHER_APP_KEY as string,
+//   secret: process.env.NEXT_PUBLIC_PUSHER_APP_SECRET as string,
+//   cluster: process.env.NEXT_PUBLIC_PUSHER_APP_CLUSTOR as string,
+//   useTLS: true,
+// });
+
+export const getMonthName = (month: number) => {
+  return month == 1
+    ? "Jan"
+    : month == 2
+    ? "Feb"
+    : month == 3
+    ? "Mar"
+    : month == 4
+    ? "Apr"
+    : month == 5
+    ? "May"
+    : month == 6
+    ? "Jun"
+    : month == 7
+    ? "Jul"
+    : month == 8
+    ? "Aug"
+    : month == 9
+    ? "Sep"
+    : month == 10
+    ? "Oct"
+    : month == 11
+    ? "Nov"
+    : month == 12 && "Dec";
+};
