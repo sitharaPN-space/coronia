a8705eb995ef08c4d2618909d6b69a328ea85cf6
Sithara <sitharapitiyage@gmail.com>
2024-07-18 16:08:21 +0530

    AI bot implementation is started

---
+++ src/app/chatbot/page.tsx
@@ -0,0 +1,9 @@
+import AIChatBot from "@/components/chatbot/index";
+
+type Props = {};
+
+const ChatBot = (props: Props) => {
+  return <AIChatBot />;
+};
+
+export default ChatBot;
---
+++ src/components/chatbot/index.tsx
@@ -0,0 +1,8 @@
+"use client";
+type Props = {};
+
+const AIChatBot = (props: Props) => {
+  return <div>AiChatBot From Sithara</div>;
+};
+
+export default AIChatBot;
