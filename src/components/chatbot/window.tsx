ec07af051f555fa8c929fb392cb8061f6a7e604d
Sithara <sitharapitiyage@gmail.com>
2024-07-22 11:11:55 +0530

    AI chat integration is completed

--- src/components/chatbot/window.tsx
+++ src/components/chatbot/window.tsx
@@ -1,9 +1,21 @@
+import { BOT_TABS_MENU } from "@/constants/menu";
 import { ChatBotMessageProps } from "@/schemas/conversation.schema";
+import { Paperclip, Send } from "lucide-react";
 import Image from "next/image";
 import { forwardRef } from "react";
 import { UseFormRegister } from "react-hook-form";
+import Accordion from "../accordian/index";
+import TabsMenu from "../tabs/index";
 import { Avatar, AvatarFallback, AvatarImage } from "../ui/avatar";
+import { Button } from "../ui/button";
+import { CardDescription, CardTitle } from "../ui/card";
+import { Input } from "../ui/input";
+import { Label } from "../ui/label";
+import { Separator } from "../ui/separator";
+import { TabsContent } from "../ui/tabs";
+import Bubble from "./bubble";
 import RealTimeMode from "./real-time";
+import { Responding } from "./responding";
 
 type Props = {
   errors: any;
@@ -86,6 +98,73 @@ export const BotWindow = forwardRef<HTMLDivElement, Props>(
             />
           </div>
         </div>
+        <TabsMenu
+          triggers={BOT_TABS_MENU}
+          className=" bg-transparent border-[1px] border-border m-2"
+        >
+          <TabsContent value="chat">
+            <Separator orientation="horizontal" />
+            <div className="flex flex-col h-full">
+              <div
+                style={{
+                  background: theme || "",
+                  color: textColor || "",
+                }}
+                className="px-3 flex h-[400px] flex-col py-5 gap-3 chat-window overflow-y-auto"
+                ref={ref}
+              >
+                {chats.map((chat, key) => (
+                  <Bubble key={key} message={chat} />
+                ))}
+                {onResponding && <Responding />}
+              </div>
+              <form
+                onSubmit={onChat}
+                className="flex px-3 py-1 flex-col flex-1 bg-porcelain"
+              >
+                <div className="flex justify-between">
+                  <Input
+                    {...register("content")}
+                    placeholder="Type your message..."
+                    className="focus-visible:ring-0 flex-1 p-0 focus-visible:ring-offset-0 bg-porcelain rounded-none outline-none border-none"
+                  />
+                  <Button type="submit" className="mt-3">
+                    <Send />
+                  </Button>
+                </div>
+                <Label htmlFor="bot-image">
+                  <Paperclip />
+                  <Input
+                    {...register("image")}
+                    type="file"
+                    id="bot-image"
+                    className="hidden"
+                  />
+                </Label>
+              </form>
+            </div>
+          </TabsContent>
+
+          <TabsContent value="helpdesk">
+            <div className="h-[485px] overflow-y-auto overflow-x-hidden p-4 flex flex-col gap-4">
+              <div>
+                <CardTitle>Help Desk</CardTitle>
+                <CardDescription>
+                  Browse from a list of questions people usually ask.
+                </CardDescription>
+              </div>
+              <Separator orientation="horizontal" />
+
+              {helpdesk.map((desk) => (
+                <Accordion
+                  key={desk.id}
+                  trigger={desk.question}
+                  content={desk.answer}
+                />
+              ))}
+            </div>
+          </TabsContent>
+        </TabsMenu>
       </div>
     );
   }
